dbc6b62f9173f4ac95f84b9b8657357febefe62d
Lucas BRUGUIER <bruguier_lucas@yahoo.fr>
2019-07-26 23:59:09 +0200

    la case 'conditions d'utilisation' permet de verifier le formulaire et de dévérouiller l'envoie

--- html/formulaire_best_project.js
+++ html/formulaire_best_project.js
@@ -6,7 +6,6 @@ function selection_niveau_maths(){
     }
 }
 
-
 function selection_niveau_francais() {
     if (document.getElementById("Francais").checked == true){
         document.getElementById("container_francais").style.visibility="visible";
@@ -25,7 +24,6 @@ function selection_niveau_histoire() {
 
 }
 
-
 function selection_niveau_chimie() {
     if (document.getElementById("Chimie").checked == true){
         document.getElementById("container_chimie").style.visibility="visible";
@@ -35,40 +33,67 @@ function selection_niveau_chimie() {
 }
 
 function verif_formulaire() {
+
+    //si il y a une erreur on ne déverouille pas le bouton d'envoie
+    var erreur =0
+    // on incremente a chaque fois qu'il y a une erreur
+
     // definit un RegExp pour le telephone
     var Regegex_tel = new RegExp("^(0|\\+33|0033)[1-9][0-9]{8}$")
     if (Regegex_tel.test(document.getElementById("tel").value)) {
         document.getElementById("numero_incorrect").style.visibility="hidden";
-    } else {document.getElementById("numero_incorrect").style.visibility="visible";}
+    } else {
+        document.getElementById("numero_incorrect").style.visibility="visible";
+        erreur +=1;
+    }
     // affiche le message d'erreur du tel
     
     var Regegex_mail = new RegExp("^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$")
     if ((Regegex_mail.test(document.getElementById("email").value)) || (document.getElementById("email").value === null)) {
         document.getElementById("email_incorrect").style.visibility="hidden";
-    } else {document.getElementById("email_incorrect").style.visibility="visible";}
+    } else {
+        document.getElementById("email_incorrect").style.visibility="visible";
+        erreur +=1;
+    }
     // le || permet de faire un OR? bref ça marche pas si l'email est vide
     
 // pour le mot de passe je verifie qu'il y ai 1 minuscule (score_pass + 1), 1 majuscule, 1 nombre et 8 caractères    
     score_pass=0
     var pass = document.getElementById("pass").value
     if (pass.match(/[a-z]/)){
-        score_pass +=1
+        score_pass +=1;
     }if (pass.match(/[A-Z]/)){
-        score_pass +=1
+        score_pass +=1;
     }if (pass.match(/[0-9]/)){
-        score_pass +=1
+        score_pass +=1;
     }if (pass.length > 7){
-        score_pass +=1
+        score_pass +=1;
     }
     if (score_pass == 4){
         document.getElementById("password_incorrect").style.visibility="hidden";
-    } else {document.getElementById("password_incorrect").style.visibility="visible";}
+    } else {
+        document.getElementById("password_incorrect").style.visibility="visible";
+        erreur +=1;
+    }
+//donc si le score final est =4 c'est que toutes les conditions sont respectées
 
+
+// on calcule le nombre d'année d'écart et on affiche le message d'erreur (la division sert à transformer la difference en seconde en difference en année)
     d1= new Date()
     d2= new Date(document.querySelector('input[type="date"]').value)
     if (Number((d1.getTime() - d2.getTime()) / 31536000000).toFixed(0) < 18 ||Number((d1.getTime() - d2.getTime()) / 31536000000).toFixed(0) >100) {
         document.getElementById("age_incorrect").style.visibility="visible";
+        erreur +=1;
     } else {
         document.getElementById("age_incorrect").style.visibility="hidden";
+    }
+
+
+    //si il n'y a aucune erreur on autorise l'envoie du formulaire, sinon on décoche la case (et on fait apparaitre les differents messages d'erreur)
+    if (erreur==0){
+        document.getElementById("bouton_d_envoie").disabled = false
+    } else {
+        document.getElementById("verification").checked = false
+        document.getElementById("bouton_d_envoie").disabled = true
     }
 }
